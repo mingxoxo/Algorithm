78983cf83825d547e43997c7c4b8a9f662a2e10d
jeongmin <44529556+mingxoxo@users.noreply.github.com>
2020-10-11 02:35:59 +0900

    Create 큰 수의 법칙.py

---
+++ study/그리디/큰 수의 법칙.py
@@ -0,0 +1,16 @@
+#큰수의 법칙
+#첫째 줄에 배열의 크기 N, 숫자가 더해지는 수 M, K 초과 금지
+
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+if first == second:
+  answer = m*first
+else:
+  answer = (m//k)*second + (m - (m//k))*first
+
+print(answer)
