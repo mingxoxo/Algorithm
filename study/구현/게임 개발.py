972d01a2911a514c3ba4cdecab1e9b4f324e65b2
jeongmin <44529556+mingxoxo@users.noreply.github.com>
2021-11-10 16:49:21 +0900

    Update 게임 개발.py

--- study/구현/게임 개발.py
+++ study/구현/게임 개발.py
@@ -6,37 +6,42 @@
 # 0 : 북쪽, 1 : 동쪽, 2 : 남쪽, 3 : 서쪽
 move = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
+
 def visited_map(game_map, x, y, direction, visited, cnt):
     count = 0
     while True:
-        while count < 4 :
+        while count < 4:
+            print(x, y, direction)
             count += 1
             direction = (direction + 3) % 4  # 왼쪽 방향
             dx = move[direction][0]
             dy = move[direction][1]
             try:
-                if visited[x+dx][y+dy] is False: #왼쪽방향 안가봤으면
-                    visited[x][y] = True
+                if visited[x + dx][y + dy] is False:  # 왼쪽방향 안가봤으면
+                    visited[x + dx][y + dy] = True
                     cnt += 1
-                    x = x+dx
-                    y = y+dy
+                    x = x + dx
+                    y = y + dy
                     count = 0
             except:
                 continue
         try:
-            if visited[x-dx][y-dx] is True:
-                if game_map[x-dx][y-dx] == 1:
+            if visited[x - dx][y - dy] is True:
+                if game_map[x - dx][y - dy] == 1:
                     break
                 else:
-                    x = x-dx
-                    y = y-dx
+                    x = x - dx
+                    y = y - dy
+                    count = 0
         except:
             break
     return cnt
 
 
 N, M = map(int, input().split())
+print(N, M)
 firstX, firstY, direction = map(int, input().split())
+print(firstX, firstY, direction)
 cnt = 1
 
 game_map = []
@@ -45,4 +50,5 @@ for i in range(N):
     game_map.append(list(map(int, input().split())))
     visited.append([bool(int(x)) for x in game_map[i]])  # 외우자
 
+visited[firstX][firstY] = True
 print(visited_map(game_map, firstX, firstY, direction, visited, cnt))
