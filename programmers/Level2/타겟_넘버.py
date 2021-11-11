506c7e578b358e4f4e360e15aa5ae5fb9c094a2d
jeongmin <44529556+mingxoxo@users.noreply.github.com>
2021-11-11 09:35:04 +0900

    Update 타겟_넘버.py

--- programmers/Level2/타겟_넘버.py
+++ programmers/Level2/타겟_넘버.py
@@ -14,3 +14,14 @@ def dfs(numbers, target, answer, index, result):
 def solution(numbers, target):
     answer = dfs(numbers, target, 0, 0, 0)
     return answer
+
+## 다른 사람의 풀이 중 깔끔하게 재귀로 푼 코드  
+# def solution(numbers, target):
+#     if not numbers and target == 0: #만약 리스트가 비어있고 target 값이 0이 되면 1 반환
+#         return 1
+#     elif not numbers: #리스트가 비었는데 target이 0이 아니라면 0 반환
+#         return 0
+    
+#     answer = solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0]) #
+    
+#     return answer
