56a56dcd7bd1caf2a6eae723dd96904bddbada22
jeongmin <44529556+mingxoxo@users.noreply.github.com>
2021-12-02 14:38:28 +0900

    Update 조이스틱.py

--- programmers/Level2/조이스틱.py
+++ programmers/Level2/조이스틱.py
@@ -17,12 +17,9 @@ def solution(name):
                 A_list.append(len(A_str))
             A_list.append(0)
             A_str = []
-    print(A_list)
     
     l_c = A_list.index(max(A_list))-1 if A_list.index(max(A_list)) != 0 else 0 
     r_c = len(name) - (A_list.index(max(A_list))+max(A_list))
     min_count = min([len(name)-1, l_c*2+r_c, r_c*2+l_c])
-    
-    print(l_c, r_c)
     
     return sum(make_name)+min_count
