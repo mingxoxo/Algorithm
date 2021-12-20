223d67f6d255ee318b25911e5a222b68ba4d3cd9
jeongmin <44529556+mingxoxo@users.noreply.github.com>
2021-12-20 09:53:55 +0900

    위클리 챌린지 > 부족한 금액 계산하기

--- programmers/Level1/82612.py
+++ programmers/Level1/82612.py
@@ -4,3 +4,6 @@ def solution(price, money, count):
     answer = money - (count*(count+1)/2 * price)
     answer = - answer if answer < 0 else 0
     return answer
+
+#다른 사람의 풀이 --> 한 줄로 가능
+#return max(0,price*(count+1)*count//2-money) 
