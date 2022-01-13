7c5a389a6405711931923bfdb2ec59b3877d44f4
jeongmin <yesuel1111@naver.com>
2022-01-13 10:30:53 +0900

    탐욕법(Greedy) > 큰 수 만들기 (시간초과 해결)

--- programmers/Level2/42883.py
+++ programmers/Level2/42883.py
@@ -1,15 +1,20 @@
-#큰 수 만들기 (테스트케이스 10번 시간초과)
+#큰 수 만들기
 #https://programmers.co.kr/learn/courses/30/lessons/42883
 
+#stack으로 코드 구현 - 테스트케이스 10번 시간초과 해결
+#참고 - https://programmers.co.kr/questions/12094
+
 def solution(number, k):
-    answer = ""
+    answer = []
     num_list = list(number)
-    for i in range(len(number)-k, 0, -1):
-        l = len(num_list)
-        if i == l:
-            answer += ''.join(num_list)
-            break
-        answer += max(num_list[:l-i+1])
-        num_list = num_list[num_list.index(answer[-1])+1:]
+    cnt = 0
+    for i in range(len(number)):
+        while answer and answer[-1] < num_list[i]:
+            answer.pop()
+            cnt += 1
+            if cnt == k:
+                answer += num_list[i:]
+                return ''.join(answer)
+        answer.append(num_list[i])
     
-    return answer
+    return ''.join(answer[:-k]) #같은 수가 계속 반복되면 삭제가 되지 않게됨
