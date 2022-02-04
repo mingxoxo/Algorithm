aef4b16360e3d94f811062eb2e7d48a9242bf559
jeongmin <yesuel1111@naver.com>
2022-02-04 22:45:30 +0900

    해시 > 전화번호 목록

--- programmers/Level2/42577.py
+++ programmers/Level2/42577.py
@@ -1,23 +1,13 @@
 #전화번호 목록
-#효율성 3번, 4번 시간초과
+#효율성 3번, 4번 통과
+#참고 : https://programmers.co.kr/questions/25120
 
-#1
 def solution(phone_book):
-    phone_book.sort()
-    for i in range(len(phone_book)):
-        for j in range(i+1, len(phone_book)):
-            if len(phone_book[j]) < len(phone_book[i]):
-                break
-            if phone_book[j].find(phone_book[i]) == 0:
-                return False
+    phone_book.sort(key = lambda x: (x, len(x)))
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].find(phone_book[i]) == 0:
+            return False
     return True
- 
 
-#2
-def solution(phone_book):
-    phone_book.sort(key = len)
-    for i in range(len(phone_book)):
-        for j in range(i+1, len(phone_book)):
-            if phone_book[j].find(phone_book[i]) == 0:
-                return False
-    return True
+#다른사람의 풀이
+#startswith, 해쉬
