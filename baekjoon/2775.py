32b5543b6844f0d8c4118c9fbc9487e46462a6b4
jeongmin <yesuel1111@naver.com>
2022-04-08 23:49:18 +0900

    Update 2775.py

--- baekjoon/2775.py
+++ baekjoon/2775.py
@@ -1,21 +1,24 @@
 #부녀회장이 될테야
 #https://www.acmicpc.net/problem/2775
 
-#Pypy3 통과
-#파이썬3은 시간초과
+#파이썬3 통과 --> 이전 값을 변수에 저장해줌
 
 #k층 n호
-def resident(k, n):
+def resident(k, n, apart):
     # 종료 조건: 0층이거나 1호일 때
     if n == 1:
         return 1
     if k == 0:
         return n
-    return resident(k-1, n) + resident(k, n-1)
+    if k not in apart[n].keys():
+        apart[n][k] = resident(k - 1, n, apart) + resident(k, n - 1, apart)
 
+    return apart[n][k]
+
+apart = [{} for i in range(15)]
 
 T = int(input())
 for i in range(T):
     k = int(input())
     n = int(input())
-    print(resident(k, n))
+    print(resident(k, n, apart))
