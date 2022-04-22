51c8444ee92721646bf59a05e24a91c981a764df
jeongmin <yesuel1111@naver.com>
2022-04-22 16:24:30 +0900

    Create 3053.py

---
+++ baekjoon/3053.py
@@ -0,0 +1,10 @@
+#택시 기하학
+#https://www.acmicpc.net/problem/3053
+
+import math
+R = int(input())
+
+#유클리드 기하학과 맨헤튼 거리의 원은 모양이 다름
+
+print("{:.6f}".format(R*R*math.pi))
+print("{:.6f}".format(R*R*2))
