74531f900f161c9d18ab96910c8c34217eb92bac
jeongmin <yesuel1111@naver.com>
2022-07-18 23:04:46 +0900

    16진수

---
+++ baekjoon/1550.py
@@ -0,0 +1,15 @@
+# 16진수
+# 22.07.18
+# https://www.acmicpc.net/problem/1550
+
+import sys
+input = sys.stdin.readline
+
+N = int(input(), 16)
+
+print(format(N))
+
+
+
+# 다른 사람의 풀이
+# print(int(c, 16))
