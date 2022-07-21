31b801d115cd525f6c9ca53ad6189c55322295f2
jeongmin <yesuel1111@naver.com>
2022-07-21 20:57:39 +0900

    특별한 날

---
+++ baekjoon/10768.py
@@ -0,0 +1,8 @@
+# 특별한 날
+# https://www.acmicpc.net/problem/10768
+
+M = int(input())
+D = int(input())
+
+day = M * 100 + D
+print("Before") if day < 218 .imag else print("Special") if day == 218 else print("After")
