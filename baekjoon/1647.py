137c9cb93350bb9956d0cef08da07250e8736364
jeongmin <yesuel1111@naver.com>
2022-12-13 12:35:18 +0900

    도시 분할 계획

---
+++ baekjoon/1647.py
@@ -0,0 +1,49 @@
+# 도시 분할 계획
+# 22.12.13
+# 최소 스패닝 트리
+# https://www.acmicpc.net/problem/1647
+
+import sys
+import heapq
+input = sys.stdin.readline
+
+
+def find(parent, v):
+    if parent[v] == v:
+        return v
+    parent[v] = find(parent, parent[v])
+    return parent[v]
+
+
+def union(parent, v1, v2):
+    root1 = find(parent, v1)
+    root2 = find(parent, v2)
+    if root1 < root2:
+        parent[root2] = root1
+    else:
+        parent[root1] = root2
+
+
+def KruskalMST(graph, N):
+    parent = [i for i in range(N + 1)]
+    result = 0
+    size = N
+    while 2 < size :
+        w, h1, h2 = heapq.heappop(graph)
+        if find(parent, h1) == find(parent, h2):
+            continue
+        union(parent, h1, h2)
+        result += w
+        size -= 1
+
+    return result
+
+
+
+graph = []
+N, M = map(int, input().split())
+for _ in range(M):
+    A, B, C = map(int, input().split())
+    heapq.heappush(graph, (C, A, B))
+
+print(KruskalMST(graph, N))
