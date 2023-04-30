f8142e3c037bc9695476653dcac37124688555c5
jeongmin <yesuel1111@naver.com>
2023-04-30 15:15:29 +0900

    인사성 밝은 곰곰이

---
+++ baekjoon/25192.py
@@ -0,0 +1,21 @@
+# 인사성 밝은 곰곰이
+# https://www.acmicpc.net/problem/25192
+
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+
+N = int(input())
+user_dict = defaultdict(int)
+result = 0
+
+for _ in range(N):
+    S = input().rstrip()
+    if S == 'ENTER':
+        user_dict = defaultdict(int)
+        continue
+    user_dict[S] += 1
+    if user_dict[S] == 1:
+        result += 1
+
+print(result)
