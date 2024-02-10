8100be62c6a91b4894ce27d98e5e48020038b8ba
jeongmin <yesuel1111@naver.com>
2024-02-10 19:52:04 +0900

    01타일

---
+++ baekjoon/1904.py
@@ -0,0 +1,14 @@
+# 01타일
+# DP
+# 24.02.10
+# https://www.acmicpc.net/problem/1904
+
+N = int(input())
+dp = [0] * (N + 2)
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, N + 1):
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+
+print(dp[N])
